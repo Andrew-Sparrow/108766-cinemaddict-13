b375096ee505a086e0fe59e062e5c7ecaec5a0bd
Andrew <8911102@gmail.com>
2020-11-28 14:40:44 +0500

    functionality to display message in case of empty list of films waas added

--- src/main.js
+++ src/main.js
@@ -12,13 +12,14 @@ import FilmCardView from "./view/film-card-view";
 import {generateFilm} from "./mock/film";
 import {calculateFilmsInFilter} from "./mock/filter";
 import FooterStatisticsView from "./view/footer-statistics-view";
+import NoFilmsView from "./view/no-films";
 
 import {
   render,
   RenderPosition,
 } from "./utils/render-utils";
 
-const FILMS_COUNT = 7;
+const FILMS_COUNT = 8;
 const FILMS_COUNT_PER_STEP = 5;
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilm);
@@ -71,47 +72,53 @@ const renderPopup = (film) => {
     popupComponent.removeElement();
   });
 
-  // как лучше сделать отрисовку popup'a, так
   render(document.body, popupComponent.getElement(), RenderPosition.BEFOREEND);
-  // или так ?
-  // document.body.appendChild(popupComponent.getElement());
 };
 
 render(siteHeaderElement, new UserProfileView().getElement(), RenderPosition.BEFOREEND);
 render(siteMainElement, new MainNavigationView(filters).getElement(), RenderPosition.BEFOREEND);
 
-render(siteMainElement, new SortMenuView().getElement(), RenderPosition.BEFOREEND);
-
 const filmsComponent = new FilmsView();
-
-render(siteMainElement, filmsComponent.getElement(), RenderPosition.BEFOREEND);
 
 render(footer, new FooterStatisticsView(films.length).getElement(), RenderPosition.BEFOREEND);
 
 const filmList = filmsComponent.getElement().querySelector(`.films-list`);
 const filmListContainer = filmsComponent.getElement().querySelector(`.films-list__container`);
 
-for (let i = 1; i < Math.min(films.length, FILMS_COUNT_PER_STEP + 1); i++) {
-  renderFilmCard(filmListContainer, films[i]);
+
+if (films.length === 0) {
+
+  render(filmList, new NoFilmsView().getElement(), RenderPosition.BEFOREEND);
+
+} else {
+  render(siteMainElement, new SortMenuView().getElement(), RenderPosition.BEFOREEND);
+  render(siteMainElement, filmsComponent.getElement(), RenderPosition.BEFOREEND);
+
+  for (let i = 1; i < Math.min(films.length, FILMS_COUNT_PER_STEP + 1); i++) {
+    renderFilmCard(filmListContainer, films[i]);
+  }
+
+  if (films.length > FILMS_COUNT_PER_STEP) {
+    let renderedFilmCount = FILMS_COUNT_PER_STEP + 1;
+
+    render(filmList, new ShowMoreView().getElement(), RenderPosition.BEFOREEND);
+
+    const loadMoreButton = filmList.querySelector(`.films-list__show-more`);
+
+    loadMoreButton.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      films
+        .slice(renderedFilmCount, renderedFilmCount + FILMS_COUNT_PER_STEP)
+        // .forEach((film) => render(filmListContainer, new FilmCardView(film).getElement(), RenderPosition.BEFOREEND));
+        .forEach((film, index) => renderFilmCard(filmListContainer, films[index]));
+
+      renderedFilmCount += FILMS_COUNT_PER_STEP;
+
+      if (renderedFilmCount >= films.length) {
+        loadMoreButton.remove();
+      }
+    });
+  }
 }
 
-if (films.length > FILMS_COUNT_PER_STEP) {
-  let renderedFilmCount = FILMS_COUNT_PER_STEP + 1;
 
-  render(filmList, new ShowMoreView().getElement(), RenderPosition.BEFOREEND);
-
-  const loadMoreButton = filmList.querySelector(`.films-list__show-more`);
-
-  loadMoreButton.addEventListener(`click`, (evt) => {
-    evt.preventDefault();
-    films
-      .slice(renderedFilmCount, renderedFilmCount + FILMS_COUNT_PER_STEP)
-      .forEach((film) => render(filmListContainer, new FilmCardView(film).getElement(), RenderPosition.BEFOREEND));
-
-    renderedFilmCount += FILMS_COUNT_PER_STEP;
-
-    if (renderedFilmCount >= films.length) {
-      loadMoreButton.remove();
-    }
-  });
-}
