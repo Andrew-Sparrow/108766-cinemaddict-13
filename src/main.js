dcb64de610fc35ecfb3cff04e8f2cb19e52064e3
Andrew <8911102@gmail.com>
2020-12-01 10:58:26 +0500

    function renderBoard was added

--- src/main.js
+++ src/main.js
@@ -28,47 +28,48 @@ const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 const footer = document.querySelector(`.footer`);
 
-
 render(siteHeaderElement, new UserProfileView(), RenderPosition.BEFOREEND);
 render(siteMainElement, new MainNavigationView(filters), RenderPosition.BEFOREEND);
 
-const filmsComponent = new FilmsView();
-
 render(footer, new FooterStatisticsView(films.length), RenderPosition.BEFOREEND);
 
-const filmList = filmsComponent.getElement(`.films-list`);
-const filmListContainer = filmsComponent.getElement(`.films-list__container`);
+const renderBoard = (boardFilms) => {
+  const filmsComponent = new FilmsView();
+  const filmList = filmsComponent.getElement(`.films-list`);
+  const filmListContainer = filmsComponent.getElement(`.films-list__container`);
 
+  if (boardFilms.length === 0) {
 
-if (films.length === 0) {
+    render(filmList, new NoFilmsView(), RenderPosition.BEFOREEND);
 
-  render(filmList, new NoFilmsView(), RenderPosition.BEFOREEND);
+  } else {
+    render(siteMainElement, new SortMenuView(), RenderPosition.BEFOREEND);
+    render(siteMainElement, filmsComponent, RenderPosition.BEFOREEND);
 
-} else {
-  render(siteMainElement, new SortMenuView(), RenderPosition.BEFOREEND);
-  render(siteMainElement, filmsComponent, RenderPosition.BEFOREEND);
+    for (let i = 1; i < Math.min(films.length, FILMS_COUNT_PER_STEP + 1); i++) {
+      renderFilmCard(filmListContainer, films[i]);
+    }
 
-  for (let i = 1; i < Math.min(films.length, FILMS_COUNT_PER_STEP + 1); i++) {
-    renderFilmCard(filmListContainer, films[i]);
+    if (films.length > FILMS_COUNT_PER_STEP) {
+      let renderedFilmCount = FILMS_COUNT_PER_STEP + 1;
+
+      const showMoreButton = new ShowMoreView();
+
+      render(filmList, showMoreButton, RenderPosition.BEFOREEND);
+
+      showMoreButton.setClickHandler(() => {
+        films
+          .slice(renderedFilmCount, renderedFilmCount + FILMS_COUNT_PER_STEP)
+          .forEach((film, index) => renderFilmCard(filmListContainer, films[index]));
+
+        renderedFilmCount += FILMS_COUNT_PER_STEP;
+
+        if (renderedFilmCount >= films.length) {
+          remove(showMoreButton);
+        }
+      });
+    }
   }
+};
 
-  if (films.length > FILMS_COUNT_PER_STEP) {
-    let renderedFilmCount = FILMS_COUNT_PER_STEP + 1;
-
-    const showMoreButton = new ShowMoreView();
-
-    render(filmList, showMoreButton, RenderPosition.BEFOREEND);
-
-    showMoreButton.setClickHandler(() => {
-      films
-        .slice(renderedFilmCount, renderedFilmCount + FILMS_COUNT_PER_STEP)
-        .forEach((film, index) => renderFilmCard(filmListContainer, films[index]));
-
-      renderedFilmCount += FILMS_COUNT_PER_STEP;
-
-      if (renderedFilmCount >= films.length) {
-        remove(showMoreButton);
-      }
-    });
-  }
-}
+renderBoard(films);
