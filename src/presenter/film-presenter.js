94a1ed4c44015e21ad947bc4b04512f40b8f26e1
Andrew <8911102@gmail.com>
2020-12-08 19:21:08 +0500

    5.1.10 Добавляем возможность переиспользования

--- src/presenter/film-presenter.js
+++ src/presenter/film-presenter.js
@@ -3,25 +3,46 @@ import FilmCardView from "../view/film-card-view";
 import {
   render,
   renderPopup,
+  replace,
   RenderPosition
 } from "../utils/render-utils";
+
+import {remove} from "../utils/utils";
 
 export default class FilmCardPresenter {
   constructor(filmListContainerElement) {
     this._filmListContainerElement = filmListContainerElement;
+
     this._filmCardComponent = null;
+
     this._handlePopupOpen = this._handlePopupOpen.bind(this);
   }
 
   init(film) {
     this._film = film;
+
+    const prevFilmCardComponent = this._filmCardComponent;
+
     this._filmCardComponent = new FilmCardView(film);
 
     this._filmCardComponent.setCardPosterClickHandler(this._handlePopupOpen);
     this._filmCardComponent.setCardTitleClickHandler(this._handlePopupOpen);
     this._filmCardComponent.setCardCommentsClickHandler(this._handlePopupOpen);
 
-    render(this._filmListContainerElement, this._filmCardComponent, RenderPosition.BEFOREEND);
+    if (prevFilmCardComponent === null) {
+      render(this._filmListContainerElement, this._filmCardComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    if (this._filmListContainerElement.getElement().contains(prevFilmCardComponent.getElement())) {
+      replace(this._filmCardComponent, prevFilmCardComponent);
+    }
+
+    remove(prevFilmCardComponent);
+  }
+
+  destroy() {
+    remove(this._filmCardComponent);
   }
 
   _handlePopupOpen() {
