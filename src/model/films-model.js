9ee00c1a323e7008813eda7e5eea84728d63f1f1
Andrew <8911102@gmail.com>
2021-01-08 15:31:15 +0500

    8.1.3 реализуем паттерн адаптер

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -30,4 +30,85 @@ export default class FilmsModel extends Observer {
 
     this._notify(updateType, updatedItem);
   }
+
+  static adaptToClient(filmFromServer) {
+    const adaptedFilmForClient = Object.assign(
+        {},
+        filmFromServer,
+        {
+          id: filmFromServer.id,
+          poster: filmFromServer.film_info.poster,
+          title: filmFromServer.film_info.title,
+          originalTitle: filmFromServer.film_info.alternative_title,
+          rating: filmFromServer.film_info.total_rating,
+          director: filmFromServer.film_info.director,
+          screenwriters: filmFromServer.film_info.writers,
+          actors: filmFromServer.film_info.actors,
+          releaseDate: filmFromServer.film_info.release.date,
+          duration: filmFromServer.film_info.runtime,
+          country: filmFromServer.film_info.release.release_country,
+          genres: filmFromServer.film_info.genre,
+          description: filmFromServer.film_info.description,
+          ageRating: filmFromServer.film_info.age_rating,
+          comments: filmFromServer.comments,
+          isFavorite: filmFromServer.user_details.favorite,
+          isInWatchlist: filmFromServer.user_details.watchlist,
+          isWatched: filmFromServer.user_details.already_watched,
+          watchingDate: filmFromServer.user_details.watching_date,
+        }
+    );
+
+    // Ненужные ключи мы удаляем
+    delete adaptedFilmForClient.film_info;
+    delete adaptedFilmForClient.user_details;
+
+    return adaptedFilmForClient;
+  }
+
+  static adaptToServer(filmFromClient) {
+    const filmInfoForServer = Object.assign(
+        {},
+        {
+          "title": filmFromClient.title,
+          "alternative_title": filmFromClient.originalTitle,
+          "total_rating": filmFromClient.rating,
+          "poster": filmFromClient.poster,
+          "age_rating": filmFromClient.ageRating,
+          "director": filmFromClient.director,
+          "writers": filmFromClient.screenwriters,
+          "actors": filmFromClient.actors,
+          "release": Object.assign(
+              {},
+              {
+                "date": filmFromClient.releaseDate,
+                "release_country": filmFromClient.country
+              }
+          ),
+          "runtime": filmFromClient.duration,
+          "genre": filmFromClient.genres,
+          "description": filmFromClient.description
+        }
+    );
+
+    const userDetailsForServer = Object.assign(
+        {},
+        {
+          "watchlist": filmFromClient.isInWatchlist,
+          "already_watched": filmFromClient.isWatched,
+          "watching_date": filmFromClient.watchingDate,
+          "favorite": filmFromClient.isFavorite
+        }
+    );
+
+    const adaptedFilmForServer = Object.assign(
+        {},
+        {
+          "comments": filmFromClient.comments,
+          "film_info": filmInfoForServer,
+          "user_details": userDetailsForServer
+        }
+    );
+
+    return adaptedFilmForServer;
+  }
 }
